0b6d6d6f850189c5ca271f85d05017710456b486
Ardit Konjuhi <Ardit.Konjuhi@fleetcor.com>
2024-10-01 12:21:26 +0200

    Implemented DownloadImageNotifier save the image to their gallery

--- lib/features/chat/presentation/notifiers/notifiers.dart
+++ lib/features/chat/presentation/notifiers/notifiers.dart
@@ -1,6 +1,7 @@
 export 'chat_notifier.dart';
 export 'create_chat_notifier.dart';
 export 'delete_chat_notifier.dart';
+export 'download_image_notifier.dart';
 export 'get_all_chats_notifier.dart';
 export 'home_notifier.dart';
 export 'image_list_notifier.dart';
--- lib/features/chat/presentation/screens/image_details_screen.dart
+++ lib/features/chat/presentation/screens/image_details_screen.dart
@@ -1,7 +1,9 @@
+import 'package:ai_assistant/features/chat/presentation/notifiers/download_image_notifier.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class ImageDetailScreen extends StatelessWidget {
+class ImageDetailScreen extends ConsumerWidget {
   final String imageUrl;
   final String prompt;
 
@@ -12,7 +14,9 @@ class ImageDetailScreen extends StatelessWidget {
   });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final downloadState = ref.watch(downloadImageNotifierProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -20,25 +24,43 @@ class ImageDetailScreen extends StatelessWidget {
           maxLines: 1,
           overflow: TextOverflow.ellipsis,
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.download),
+            onPressed: () async {
+              ref
+                  .read(downloadImageNotifierProvider.notifier)
+                  .downloadImage(imageUrl, prompt);
+            },
+          ),
+        ],
       ),
-      body: Center(
-        child: InteractiveViewer(
-          minScale: 0.5,
-          maxScale: 4.0,
-          child: Hero(
-            tag: imageUrl,
-            child: CachedNetworkImage(
-              imageUrl: imageUrl,
-              fit: BoxFit.contain,
-              width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height,
-              placeholder: (context, url) => const Center(
-                child: CircularProgressIndicator(),
+      body: Stack(
+        children: [
+          Center(
+            child: InteractiveViewer(
+              minScale: 0.5,
+              maxScale: 4.0,
+              child: Hero(
+                tag: imageUrl,
+                child: CachedNetworkImage(
+                  imageUrl: imageUrl,
+                  fit: BoxFit.contain,
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  placeholder: (context, url) => const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
               ),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
             ),
           ),
-        ),
+          if (downloadState is AsyncLoading)
+            const Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
       ),
     );
   }
